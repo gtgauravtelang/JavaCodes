4ca78cdb65e12c92021c9e7a0efcc7a23b1d21cd
Your Name <you@example.com>
2024-02-17 11:22:01 +0400

    Modified ReadMe file & Main

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
+        int counter = 5;
+        for (int i=0;i<counter;i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
