bd22ee822b3421991976bbe51519ff311e7c606e
Your Name <you@example.com>
2024-02-17 14:53:29 +0400

    Modified str in StringSplitCode

--- src/StringSplitCode.java
+++ src/StringSplitCode.java
@@ -1,7 +1,7 @@
 public class StringSplitCode {
     public static void main(String[] args)
     {
-        String str1 = "This is my Java Code File.";
+        String str1 = "This is my Java Code File & this is going to be success.";
         String[] arraystring;
 
         arraystring = str1.split(" ");
